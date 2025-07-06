22b6b3dd5d63ee1b2cd4c89e1187ef631e724d12
ahmed saeed <ahmed.s.fatahallah@gmail.com>
2025-07-06 20:28:26 +0300

    feat: add getFullPath utility and its types

---
+++ src/main.ts.ts
@@ -0,0 +1,35 @@
+import type {
+  DynamicParams,
+  ExtractDynamicSegments,
+  ParamsAsObject,
+  ReturnedPath,
+  ValidPath
+} from "./types/getFullPath";
+
+export const getFullPath = <
+  TPath extends string,
+  TParams extends
+    | [Record<ExtractDynamicSegments<TPath>[number], string>]
+    | string[]
+>(
+  path: ValidPath<TPath>,
+  ...params: ParamsAsObject<TPath, TParams> | DynamicParams<TPath, TParams>
+): ReturnedPath<TPath, TParams> => {
+  let index = 0;
+
+  return path.replace(/:\w*/g, (match) => {
+    const currentIndex = index++;
+
+    if (
+      params.length === 1 &&
+      params[0] !== null &&
+      typeof params[0] === "object"
+    ) {
+      return params[0][match.slice(1) as ExtractDynamicSegments<TPath>[number]];
+    }
+
+    return typeof params[currentIndex] === "string"
+      ? params[currentIndex]
+      : match;
+  }) as ReturnedPath<TPath, TParams>;
+};
---
+++ src/types/getFullPath.ts
@@ -0,0 +1,103 @@
+// Check if the route format is valid
+type ValidateDynamicPath<TPath extends string> =
+  TPath extends `${infer F}/${infer R}`
+    ? R extends `/${string}`
+      ? never
+      : F extends `:${string}`
+        ? `${F}/${ValidateDynamicPath<R>}`
+        : `${F}/${ValidateDynamicPath<R>}`
+    : TPath;
+
+// Replace path dynamic slugs with their respective values
+type ReturnedPathWithParams<
+  TPath extends string,
+  TParams extends string[]
+> = TPath extends `${infer First}/${infer Rest}`
+  ? First extends `:${string}`
+    ? TParams extends [
+        infer FirstParam extends string,
+        ...infer RestParams extends string[]
+      ]
+      ? ReturnedPathWithParams<`${FirstParam}/${Rest}`, RestParams>
+      : never
+    : `${First}/${ReturnedPathWithParams<Rest, TParams>}`
+  : TParams extends [infer LastParam extends string]
+    ? LastParam
+    : never;
+
+// Returned Path when the parameters are in object
+type ReturnedPathWithObjParams<
+  TPath extends string,
+  TParams extends [Record<ExtractDynamicSegments<TPath>[number], string>]
+> = TPath extends `${infer First}/${infer Rest}`
+  ? First extends `:${infer ParamName}`
+    ? ReturnedPathWithObjParams<`${TParams[0][ParamName]}/${Rest}`, TParams>
+    : `${First}/${ReturnedPathWithObjParams<Rest, TParams>}`
+  : TPath extends `:${infer LastParam}`
+    ? `${TParams[0][LastParam]}`
+    : never;
+
+// extract dynamic slugs from the path
+
+// Make sure that the provided route params match the number of dynamic slugs
+type ValidateDynamicParams<
+  TPath extends string,
+  TParams extends string[] | [Record<string, string>]
+> = TParams extends string[]
+  ? TParams["length"] extends ExtractDynamicSegments<TPath>["length"]
+    ? TParams
+    : never
+  : never;
+
+export type ExtractDynamicSegments<
+  TPath extends string,
+  TSlugs extends string[] = []
+> = TPath extends `${infer F}/${infer R}`
+  ? F extends `:${infer TSlugName}`
+    ? ExtractDynamicSegments<R, [...TSlugs, TSlugName]>
+    : ExtractDynamicSegments<R, [...TSlugs]>
+  : TPath extends `:${infer TSlugName}`
+    ? [...TSlugs, TSlugName]
+    : TPath extends ""
+      ? TSlugs
+      : never;
+
+// Check if the provided route matched the correct format
+export type ValidPath<TPath extends string> =
+  TPath extends ValidateDynamicPath<TPath> ? TPath : never;
+
+// Type params when provided as an object
+export type ParamsAsObject<
+  TPath extends string,
+  TParams extends
+    | [Record<ExtractDynamicSegments<TPath>[number], string>]
+    | string[]
+> = TParams extends [Record<ExtractDynamicSegments<TPath>[number], string>]
+  ? [
+      {
+        [K in ExtractDynamicSegments<TPath>[number]]: TParams[0][K];
+      }
+    ]
+  : never;
+
+// Check if the provided params are valid
+export type DynamicParams<
+  TPath extends string,
+  TParams extends string[] | [Record<string, string>]
+> = TParams extends string[]
+  ? TParams extends ValidateDynamicParams<TPath, TParams>
+    ? TParams
+    : ExtractDynamicSegments<TPath>
+  : never;
+
+// Final returned value
+export type ReturnedPath<
+  TPath extends string,
+  TParams extends
+    | string[]
+    | [Record<ExtractDynamicSegments<TPath>[number], string>]
+> = TParams extends string[]
+  ? ReturnedPathWithParams<TPath, TParams>
+  : TParams extends [Record<ExtractDynamicSegments<TPath>[number], string>]
+    ? ReturnedPathWithObjParams<TPath, TParams>
+    : never;
